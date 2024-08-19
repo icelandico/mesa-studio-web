7386b70eb916b3852786bd0932148db317714784
Michal <hi@geodev.me>
2024-08-19 22:02:26 +0200

    feat: set active sections

--- src/lib/components/navigation/navigation.svelte
+++ src/lib/components/navigation/navigation.svelte
@@ -1,4 +1,13 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
+	let scrollTopValue: number;
+	let activeSection: string;
+	let sectionStudio: DOMRect | undefined;
+	let sectionProject: DOMRect | undefined;
+	let sectionContact: DOMRect | undefined;
+	$: setActiveSection(scrollTopValue);
+
 	function scrollIntoView({ target }) {
 		const el = document.querySelector(target.getAttribute('href'));
 		if (!el) return;
@@ -6,30 +15,81 @@
 			behavior: 'smooth'
 		});
 	}
+
+	function setActiveSection(scrollValue: number) {
+		const scrollTopValue = scrollValue + 80;
+		console.log('Scroll', scrollValue);
+		if (
+			sectionStudio &&
+			scrollTopValue >= sectionStudio.top &&
+			scrollTopValue <= sectionStudio.bottom
+		) {
+			activeSection = 'studio';
+			return;
+		}
+		if (
+			sectionContact &&
+			scrollTopValue >= sectionContact.top &&
+			scrollTopValue <= sectionContact.bottom
+		) {
+			activeSection = 'contact';
+			return;
+		}
+		if (
+			sectionProject &&
+			scrollTopValue >= sectionProject.top &&
+			scrollTopValue <= sectionProject.bottom
+		) {
+			activeSection = 'projects';
+			return;
+		}
+	}
+
+	onMount(() => {
+		sectionStudio = document.querySelector('#studio')?.getBoundingClientRect();
+		sectionProject = document.querySelector('#projects')?.getBoundingClientRect();
+		sectionContact = document.querySelector('#contact')?.getBoundingClientRect();
+	});
 </script>
 
-<header class="fixed w-full">
+<svelte:window bind:scrollY={scrollTopValue} />
+
+<header class="fixed w-full z-10">
 	<nav>
 		<ul
 			class="h-[80px] flex bg-white justify-center items-center border border-2 border-gray-500 p-6 text-xl text-light text-primaryBlue"
 		>
 			<li class="flex-1">
-				<a class="cursor-pointer" href="#studio" on:click|preventDefault={scrollIntoView}>studio</a>
+				<a
+					class="cursor-pointer {activeSection === 'studio' ? 'active' : ''}"
+					href="#studio"
+					on:click|preventDefault={scrollIntoView}>studio</a
+				>
 			</li>
 			<li class=" flex-1">
-				<a class="cursor-pointer" href="#projects" on:click|preventDefault={scrollIntoView}
-					>projekty</a
+				<a
+					class="cursor-pointer {activeSection === 'projects' ? 'active' : ''}"
+					href="#projects"
+					on:click|preventDefault={scrollIntoView}>projekty</a
 				>
 			</li>
 			<li class="cursor-pointer flex justify-center basis-6/12">
 				<p>LOGO</p>
 			</li>
 			<li class="cursor-pointer flex-1 text-right">
-				<a class="cursor-pointer" href="#contact" on:click|preventDefault={scrollIntoView}
-					>kontakt</a
+				<a
+					class="cursor-pointer {activeSection === 'contact' ? 'active' : ''}"
+					href="#contact"
+					on:click|preventDefault={scrollIntoView}>kontakt</a
 				>
 			</li>
 			<li class="cursor-pointer flex-1 text-right">en</li>
 		</ul>
 	</nav>
 </header>
+
+<style>
+	.active {
+		border-bottom: 3px solid red;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,35 @@
-<script>
+<script lang="ts">
 	import FacebookIcon from '$lib/components/icons/facebookIcon.svelte';
 	import InstagramIcon from '$lib/components/icons/instagramIcon.svelte';
+	let element: any;
+	let clientHeight: any;
+
+	$: console.log('height', clientHeight);
 </script>
 
 <section
+	id="home"
+	class="box-border pt-[80px] h-screen bg-gray-100 border-8 border-sky-600 flex items-center justify-center"
+>
+	HOME
+</section>
+<section
 	id="studio"
-	class="box-border pt-[80px] h-screen bg-gray-200 border-8 border-green-600 flex items-center justify-center"
+	bind:this={element}
+	bind:clientHeight
+	class="box-border pt-[80px] h-screen bg-gray-200 border-8 border-lime-400 flex items-center justify-center"
 >
 	STUDIO
 </section>
 <section
 	id="projects"
-	class="box-border pt-[80px] h-screen bg-gray-200 border-8 border-tan-400 flex items-center justify-center"
+	class="box-border pt-[80px] h-screen bg-gray-300 border-8 border-indigo-400 flex items-center justify-center"
 >
 	PROJECTS
 </section>
 <section
 	id="contact"
-	class="box-border pt-[80px] h-screen bg-gray-200 border-8 border-zinc-700 flex items-center justify-center"
+	class="box-border pt-[80px] h-screen bg-gray-400 border-8 border-zinc-700 flex items-center justify-center"
 >
 	contact
 </section>
