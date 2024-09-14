aa6f354ba0e5c37fb99106768a7a3fbb689fa2bc
Michal <hi@geodev.me>
2024-09-14 10:15:46 +0200

    add parglide

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
+import { paraglide } from '@inlang/paraglide-js-adapter-sveltekit/vite';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()]
+	plugins: [
+		enhancedImages(),
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	]
 });
